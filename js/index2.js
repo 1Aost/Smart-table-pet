3117f77a8fe9dba108478eae96aec1d4889f8625
qinjunjie0212 <109152366+qinjunjie0212@users.noreply.github.com>
2023-05-08 21:17:33 +0800

    页面三-2

--- js/index2.js
+++ js/index2.js
@@ -1,18 +1,22 @@
-window.onload=()=>{
-    let voice=document.querySelector(".voice");
-    let camera=document.querySelector(".camera");
-    voice.addEventListener("click",()=>{
+window.onload = () => {
+    let voice = document.querySelector(".voice");
+    let camera = document.querySelector(".camera");
+    let set = document.querySelector(".settings")
+    voice.addEventListener("click", () => {
         const regex1 = /\/images\/ICONS\/mic\.png$/;
-        let url=voice.children[0].src;
-        if(regex1.test(url)) {
-            voice.children[0].src="../images/ICONS/mic2.png"
-        }else voice.children[0].src="../images/ICONS/mic.png"
+        let url = voice.children[0].src;
+        if (regex1.test(url)) {
+            voice.children[0].src = "../images/ICONS/mic2.png"
+        } else voice.children[0].src = "../images/ICONS/mic.png"
     })
-    camera.addEventListener("click",()=>{
+    camera.addEventListener("click", () => {
         const regex2 = /\/images\/ICONS\/cam\.png$/;
-        let url=camera.children[0].src;
-        if(regex2.test(url)) {
-            camera.children[0].src="../images/ICONS/cam2.png"
-        }else camera.children[0].src="../images/ICONS/cam.png"
+        let url = camera.children[0].src;
+        if (regex2.test(url)) {
+            camera.children[0].src = "../images/ICONS/cam2.png"
+        } else camera.children[0].src = "../images/ICONS/cam.png"
+    })
+    set.addEventListener('click', function() {
+        location.href = "./set.html"
     })
 }
--- js/set.js
+++ js/set.js
@@ -22,7 +22,7 @@ box.addEventListener('touchstart', function(e) {
 box.addEventListener('touchmove', function(e) {
         var touch = e.touches[0]
         this.endY = this.startY - touch.pageY;
-        if ((currentY - this.endY) >= -150 && (currentY - this.endY) <= 250) {
+        if ((currentY - this.endY) >= -110 && (currentY - this.endY) <= 256) {
             ul.style.top = `${currentY - this.endY}px`
         }
         for (let i = 0; i < lis.length; i++) {
@@ -32,22 +32,28 @@ box.addEventListener('touchmove', function(e) {
             // console.log(`第 ${i+1} 个 li 元素相对于父容器的：`);
             // console.log(`- 上边缘距离：${liRect.top}`);
             // console.log(`- 下边缘距离：${liRect.bottom}`);
-            if (liRect.top < 94 || liRect.top > 364) {
+            if (liRect.top < 20 || liRect.top > 428) {
+                li.style.visibility = 'hidden';
+            } else if (liRect.top < 94 || liRect.top > 364) {
                 li.style.width = 150 + 'px';
                 li.style.marginLeft = 0 + 'px';
                 li.style.borderColor = 'rgb(77, 77, 77)';
+                li.style.visibility = 'visible';
             } else if (liRect.top > 94 && liRect.top <= 148 || liRect.top > 310 && liRect.top <= 364) {
                 li.style.width = 200 + 'px';
                 li.style.marginLeft = -25 + 'px';
                 li.style.borderColor = 'rgb(77, 77, 77)';
+                li.style.visibility = 'visible';
             } else if (liRect.top > 148 && liRect.top <= 202 || liRect.top > 256 && liRect.top <= 310) {
                 li.style.width = 250 + 'px';
                 li.style.marginLeft = -50 + 'px';
                 li.style.borderColor = 'white';
+                li.style.visibility = 'visible';
             } else if (liRect.top > 202 && liRect.top <= 266) {
                 li.style.width = 300 + 'px';
                 li.style.marginLeft = -75 + 'px';
                 li.style.borderColor = 'white';
+                li.style.visibility = 'visible';
             }
         }
     })
