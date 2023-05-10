5aef00468bde3093331590531e813dbcd33036de
qinjunjie0212 <109152366+qinjunjie0212@users.noreply.github.com>
2023-05-10 21:58:17 +0800

    -2

--- js/set.js
+++ js/set.js
@@ -14,9 +14,6 @@ box.addEventListener('touchstart', function(e) {
         const li = lis[i];
         const liRect = li.getBoundingClientRect();
 
-        console.log(`第 ${i+1} 个 li 元素相对于父容器的：`);
-        console.log(`- 上边缘距离：${liRect.top}`);
-        console.log(`- 下边缘距离：${liRect.bottom}`);
     }
 })
 box.addEventListener('touchmove', function(e) {
@@ -29,31 +26,28 @@ box.addEventListener('touchmove', function(e) {
             const li = lis[i];
             const liRect = li.getBoundingClientRect();
 
-            // console.log(`第 ${i+1} 个 li 元素相对于父容器的：`);
-            // console.log(`- 上边缘距离：${liRect.top}`);
-            // console.log(`- 下边缘距离：${liRect.bottom}`);
             if (liRect.top < 20 || liRect.top > 428) {
                 li.style.visibility = 'hidden';
             } else if (liRect.top < 94 || liRect.top > 364) {
                 li.style.width = 150 + 'px';
                 li.style.marginLeft = 0 + 'px';
-                li.style.borderColor = 'rgb(77, 77, 77)';
                 li.style.visibility = 'visible';
+                li.style.opacity = 0.4;
             } else if (liRect.top > 94 && liRect.top <= 148 || liRect.top > 310 && liRect.top <= 364) {
                 li.style.width = 200 + 'px';
                 li.style.marginLeft = -25 + 'px';
-                li.style.borderColor = 'rgb(77, 77, 77)';
                 li.style.visibility = 'visible';
+                li.style.opacity = 0.6;
             } else if (liRect.top > 148 && liRect.top <= 202 || liRect.top > 256 && liRect.top <= 310) {
                 li.style.width = 250 + 'px';
                 li.style.marginLeft = -50 + 'px';
-                li.style.borderColor = 'white';
                 li.style.visibility = 'visible';
+                li.style.opacity = 0.8;
             } else if (liRect.top > 202 && liRect.top <= 266) {
                 li.style.width = 300 + 'px';
                 li.style.marginLeft = -75 + 'px';
-                li.style.borderColor = 'white';
                 li.style.visibility = 'visible';
+                li.style.opacity = 1;
             }
         }
     })
